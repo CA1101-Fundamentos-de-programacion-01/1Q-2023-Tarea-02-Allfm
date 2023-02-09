cabebbffcdf1e7087784ae4cd034b0c3df41a4bd
Allfm <124815621+Allfm@users.noreply.github.com>
2023-02-09 01:11:06 +0000

    Entrega de tarea

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ def calcularDispositivoBebe(edad, peso, altura):
     print("Portabebé")
   elif(1 <= edad <= 4 and 9 <= peso <= 18 and 75 <= altura <= 110):
     print("Silla")
+  elif(4 <= edad <=6 and 15 <= peso <= 25 and 110 <= altura <= 145):
+    print("Asiento con respaldar")
+  elif(6 <= edad <= 12 and 22 <= peso <= 36 and 110 <= altura <= 145):
+    print("Asiento sin respaldar")
+
+
+
+calcularDispositivoBebe(0.5, 9, 60)
+calcularDispositivoBebe(2, 12, 75)
+calcularDispositivoBebe(5, 20, 135)
+calcularDispositivoBebe(10, 36, 145)
+calcularDispositivoBebe(4, 16, 110)
